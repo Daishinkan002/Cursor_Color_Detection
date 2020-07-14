5648294b3251f2dba2b8ba574a03dc0c937fa962
Shantanu Tripathi <40469481+Daishinkan002@users.noreply.github.com>
2020-07-15 01:53:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
 import tkinter as tk
 from PIL import ImageTk
 from PIL import Image
-
-
-
-'''
-def Upload(Frame):
-    def __init__(self, master, *pargs):
-        Frame.__init__(self, master, *pargs)
-        self.image=Image.open("hero_h.png")
-        self.image_copy=self.image.copy()
-    
-    def _resize_image(self, event):
-        new_width=event.width
-        new_height = event.new_height
-        self.image = self.image_copy.resize((new_width,new_height))
-        self.image.grid(row = 0,column=3,rowspan=2,columnspan=5)
-
-'''
-
-
+from tkinter import filedialog as fd 
+import cv2
 
 
 width=900
 height=500
-
+flag = False
 
 def Upload():
-    image = Image.open("hero_h.png")
+    name = fd.askopenfilename(title = "Select file", filetypes = ((("png files","*.png"),("jpeg files","*.jpg"))))
+    #print(name)
+    show_image(name)
+    
+
+def Capture(flag = False):
+    cap = cv2.VideoCapture(0)
+    cap.set(3,640)
+    cap.set(4,480)
+    loop = True
+    while(loop):
+        ret,frame = cap.read()
+
+        if not ret:
+            print("Failed to grab frame")
+            break
+        cv2.imshow("Camera",frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            cv2.destroyAllWindows()
+            break
+        elif(key == ord('s')):
+            cv2.imwrite("captured_image.png",frame)
+            cv2.destroyAllWindows()
+            show_image("captured_image.png")
+            break
+        else:
+            continue
+    
+            
+    
+    
+
+
+def show_image(image_name):
+    flag = True
+    image = Image.open(image_name)
     image_copy = image.copy()
     image = image_copy.resize((width-200, height))
     photo = ImageTk.PhotoImage(image)
@@ -36,44 +55,27 @@ def Upload():
     label.grid(row=0,column=3,rowspan = 8, columnspan = 9)
     
 
-def load():
-    image = Image.open("scenery.png")
-    image_copy = image.copy()
-    image = image_copy.resize((width-200, height))
-    photo = ImageTk.PhotoImage(image)
-    label = tk.Label(image=photo)
-    label.image=photo
-    label.grid(row=0,column=3,rowspan = 8, columnspan = 9)
-    
-    
 
-
-def default():
-    image = Image.open("scenery.png")
-    image_copy = image.copy()
-    image = image_copy.resize((width-200, height))
-    photo = ImageTk.PhotoImage(image)
-    label = tk.Label(image=photo)
-    label.image=photo
-    label.grid(row=0,column=3,rowspan = 8, columnspan = 9)
-    
-
-
-if __name__ == "__main__":
+def main():
     width=900
     height=500
     window = tk.Tk()
     window.config(bg="black")
     window.title("Color_Detection")
     window.geometry("900x500") # Here x is not *
-
-    default()
     
+    show_image("hero_2.png")
 
 
-    load_btn = tk.Button(window, text="Load Image", width=25, activebackground="grey", activeforeground="blue",bg="red", command=load())
+    load_btn = tk.Button(window, text="Load Image", width=25, activebackground="grey", activeforeground="blue",bg="red", command=lambda:Upload())
     load_btn.grid(row=0,column=1,columnspan=2, padx=10, pady=50)
-    upload_btn = tk.Button(window, text="Upload Image", width=25, activebackground="grey", activeforeground="blue",bg="green", command=default())
-    upload_btn.grid(row=1,column=1,columnspan=2,padx=10,pady = 50)
-
+    capture_btn = tk.Button(window, text="Take Image", width=25, activebackground="grey", activeforeground="blue",bg="green", command=lambda:Capture())
+    capture_btn.grid(row=1,column=1,columnspan=2,padx=10,pady = 50)
+    process_btn = tk.Button(window, text="Process Colors", width=25, activebackground="grey", activeforeground="blue",bg="purple", command=lambda:Process())
+    process_btn.grid(row=2,column=1,columnspan=2,padx=10,pady = 50)
     window.mainloop()
+
+
+
+if __name__ == "__main__":
+    main()    
